adbde5e39a43645e7f89f808b1dc11b0107245b4
Evan Williams <evantwilliams23@gmail.com>
2025-08-27 22:06:32 -0500

    Intial server-side codebase

--- MessageEngine.java
+++ MessageEngine.java
@@ -1,9 +1,39 @@
 import java.net.Socket; // for client side interactions
 import java.net.ServerSocket; // for server side interactions
 
+import java.net.InetAddress;
+
 public class MessageEngine {
 	
 	public static void main(String[] args) {
-		System.out.print("Sending to: " + args[0] + " | Message: " + args[1] + "\n");
+
+		try {
+
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+
+		int port = 56827;
+
+		try (ServerSocket serverSocket = new ServerSocket(port);){
+
+			System.out.println("Server is listening on port " + serverSocket.getLocalPort());
+
+			serverSocket.setReuseAddress(true);
+	
+			while (true){
+
+				Socket clientSocket = serverSocket.accept();
+				InetAddress clientAddress = clientSocket.getInetAddress();
+				System.out.println("Client connected from " + clientAddress.getHostAddress());
+
+				clientSocket.close();
+			}
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
