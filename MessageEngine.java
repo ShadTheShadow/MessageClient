15690da9f805bfebd91f8b156d12f12a50246a8a
Evan Williams <evantwilliams23@gmail.com>
2025-08-29 21:32:57 -0500

    Server working message read

--- MessageEngine.java
+++ MessageEngine.java
@@ -1,6 +1,8 @@
 import java.net.Socket; // for client side interactions
 import java.net.ServerSocket; // for server side interactions
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 
 public class MessageEngine {
@@ -27,6 +29,23 @@ public class MessageEngine {
 				Socket clientSocket = serverSocket.accept();
 				InetAddress clientAddress = clientSocket.getInetAddress();
 				System.out.println("Client connected from " + clientAddress.getHostAddress());
+
+
+
+
+				BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                // Receive a message from the client
+                String message = in.readLine();
+                System.out.println("Received from client: " + message);
+
+                // Send a response
+                out.println("I AM SERVER");
+
+
+
 
 				clientSocket.close();
 			}
