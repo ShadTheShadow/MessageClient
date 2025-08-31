49866ee7c4ee0a624bbc81b09021767b78c5da77
kronkovich <maxwell.d.weston@gmail.com>
2025-08-31 15:24:50 -0500

    idk what i did

--- MessageEngine.java
+++ MessageEngine.java
@@ -7,57 +7,48 @@ import java.net.InetAddress;
 import java.io.FileWriter;
 
 public class MessageEngine {
-	
+
+	public static void serverAuth() {
+
+	}
+
 	public static void main(String[] args) {
-
-		try {
-
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-
 
 		int port = 55935;
 
-		try (ServerSocket serverSocket = new ServerSocket(port);){
+		try (ServerSocket serverSocket = new ServerSocket(port);) {
 
 			System.out.println("Server is listening on port " + serverSocket.getLocalPort());
 
 			serverSocket.setReuseAddress(true);
-	
-			while (true){
+
+			while (true) {
 
 				Socket clientSocket = serverSocket.accept();
 				InetAddress clientAddress = clientSocket.getInetAddress();
 				System.out.println("Client connected from " + clientAddress.getHostAddress());
 
+				// Authenticating client
+				
 
-
-				//Reading input from client
+				// Reading input from client
 				BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
+						new InputStreamReader(clientSocket.getInputStream()));
 
+				// Receive a message from the client
+				String message = in.readLine(); // Waits for new line
+				System.out.println("Received from client: " + message);
 
-				//Sending info to client
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-
-                // Receive a message from the client
-                String message = in.readLine(); //Waits for new line
-                System.out.println("Received from client: " + message);
-
-
-				//Writes message from client to txt file
+				// Writes message from client to txt file
 				FileWriter writer = new FileWriter("Recieved.txt");
 				writer.append(System.lineSeparator()).append(message);
 				writer.close();
 
-                // Send a response
-                //out.println("I AM SERVER");
+				// Send a response
+				// out.println("I AM SERVER");
 
 				clientSocket.close();
 			}
-
 
 		} catch (Exception e) {
 			e.printStackTrace();
